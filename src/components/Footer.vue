d1c03a53996eaf90653badccfb33199a6496dda7
iraklikeche <irakli.kechee@gmail.com>
2023-11-09 14:28:50 +0400

    Hero section complete

---
+++ src/components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="h-[10vh] bg-black py-8">
+    <div class="container mx-auto flex items-center justify-center gap-12">
+      <p class="text-xs uppercase text-white">spacex &copy; {{ date }}</p>
+      <a
+        class="text-xs uppercase text-white transition-opacity hover:opacity-60"
+        href="https://www.spacex.com/media/privacy_policy_spacex.pdf"
+        target="_blank"
+        >privacy policy</a
+      >
+    </div>
+  </footer>
+</template>
+
+<script setup>
+const date = new Date().getFullYear();
+console.log(date);
+</script>
--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -2,20 +2,32 @@
   <section
     v-for="launch in launches"
     :key="launch.key"
-    class="h-[85vh] bg-[url('https://sxcontent9668.azureedge.us/cms-assets/assets/Flight_2_Homepage_Desktop_a3d65abd76.jpg')] bg-cover bg-center bg-no-repeat"
+    class="h-screen bg-cover bg-center bg-no-repeat"
+    :style="{ backgroundImage: `url(${launch.background})` }"
   >
+    <video
+      v-if="launch.video"
+      autoplay
+      muted
+      loop
+      id="myVideo"
+      class="absolute bottom-0 right-0 -z-10 aspect-video"
+    >
+      <source :src="launch.video" type="video/mp4" />
+    </video>
     <div
       class="container mx-auto flex h-full flex-col items-start justify-end gap-4 px-24 pb-24"
     >
-      <h2 class="text-3xl font-normal uppercase text-white">Upcoming launch</h2>
+      <h2 class="text-3xl font-normal uppercase text-white">
+        {{ launch.launch }}
+      </h2>
       <h1 class="text-5xl font-bold uppercase text-white">
-        Starship's second <br />
-        flight test
+        {{ launch.desc }}
       </h1>
       <button
-        class="mt-4 border-2 px-10 py-4 text-xs font-extrabold uppercase tracking-tight text-white"
+        class="mt-4 min-w-[10rem] border-2 px-10 py-4 text-xs font-extrabold uppercase tracking-tight text-white"
       >
-        Learn More
+        {{ launch.reWatch ? "Watch" : "Learn More" }}
       </button>
     </div>
   </section>
